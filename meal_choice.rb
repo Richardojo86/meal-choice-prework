7c172d8e5f09ce7a74c9413c500a385e3ef3d751
richardojo04@gmail.com <richardojo04@gmail.com>
2018-06-02 04:11:45 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,5 +1,5 @@
 # Define a method meal_choice that returns the meal_choice that was
 # passed into it and defaults to meat.
-def meal_choice(element, default=meat)
+def meal_choice(element, defaul=meat)
   element 
 end 
